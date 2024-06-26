072e0a021f0c9d1042197d3429c7cf8547c31f0c
vuhung204 <vuhung220224@gmail.com>
2024-06-26 14:35:10 +0700

    vd70,71
    checkout.php (video 71 1:54)

---
+++ server/connection.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$conn = mysql_connect("locationhost", "root", "", "php_project")
+        or die("Couldn't connect to database");
+
+
+?>
---
+++ server/place_order.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+include('connection.php');
+
+if (isset($_POST['place_order'])    ) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $city = $_POST['city'];
+    $address = $_POST['address'];
+    $order_cost= $_SESSION['total'];
+    $order_status= "on_hold";
+    $user_id = 1;
+    $order_date = date('Y-m-d H:i:s');
+
+    $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date)
+                VALUES (?,?,?,?,?,?,?);   ");
+    $stmt->bind_param('isiisss',$order_cost,$order_status,$user_id,$phone,$city,$address,$order_date);
+    $stmt->execute();
+
+    $order_id = $stmt->inseri_id;
+
+    echo $order_id;
+
+    
+}
+
+
+?>
