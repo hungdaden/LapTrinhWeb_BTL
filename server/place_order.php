c8b3c884b9d6e1f915653220fb0be5957b161d51
vuhung204 <vuhung220224@gmail.com>
2024-06-26 23:06:58 +0700

    vd 72

--- server/place_order.php
+++ server/place_order.php
@@ -16,13 +16,43 @@ if (isset($_POST['place_order'])    ) {
 
     $stmt = $conn->prepare("INSERT INTO orders (order_cost, order_status, user_id, user_phone, user_city, user_address, order_date)
                 VALUES (?,?,?,?,?,?,?);   ");
+
     $stmt->bind_param('isiisss',$order_cost,$order_status,$user_id,$phone,$city,$address,$order_date);
-    
+
     $stmt->execute();
 
     $order_id = $stmt->inseri_id;
 
     echo $order_id;
+
+    
+    foreach ($_SESSION ['cart'] as $key => $value) {
+        
+        $product = $_SESSION['cart'] [$key];
+        $product_id = $product['product_id'];
+        $product_name = $product['product_name'];
+        $product_image = $product['product_image'];
+        $product_price = $product['product_price'];
+        $product_quantity = $product['product_quantity'];
+
+        $stmt1 = $conn->prepare("INSERT INTO order_items (order_id,product_id,product_name,product_image,product_price,product_quantity,user_id,order_date)
+                    VALUE (?,?,?,?,?,?,?,?)  ");
+        
+        $stmt1->bind_param('iissiiis',$order_id,$product_id,$product_name,$product_image,$product_price,$product_quantity,$user_id,$order_date);
+
+        $stmt1->execute();
+
+        
+
+    }
+
+
+
+    ///unset($_SESSION['cart']);
+
+    header('location: ../payment.php?order_status=order placed successfully');
+
+
 
 }
 
