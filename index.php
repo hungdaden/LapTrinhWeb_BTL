1c804f0f1e42d005b8374d52b44a350beb3bb4ec
vuhung204 <vuhung220224@gmail.com>
2024-07-03 23:35:06 +0700

    vd 75

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               
               <li class="nav-item">
-                <a class="nav-link" href="index.html">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
               </li>
               
               <li class="nav-item">
