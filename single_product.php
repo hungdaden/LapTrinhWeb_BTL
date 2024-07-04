bfafc3575944eb5d8019701942b088e93d3304d0
Hoang Hung <hunghoang00a@gmail.com>
2024-07-04 20:32:18 +0700

    update product category

--- single_product.php
+++ single_product.php
@@ -59,7 +59,7 @@ if (isset($_GET['product_id'])) {
             
 
             <div class="col-lg-6 col-md-12 col-12">
-                <h6>Men/Shoes</h6>
+                <h6><?php echo $row['product_category']; ?></h6>
                 <h3 class="py-4"><?php echo $row['product_name']; ?></h3>
                 <h2>$ <?php echo $row['product_price']; ?></h2>
 
